e449f14508a9fd6f560c7918c38a5c15e6832c42
Pedro Barros <pedrobars7a@gmail.com>
2024-08-07 14:07:05 -0300

    validate check in

---
+++ src/use-cases/validate-check-in.ts
@@ -0,0 +1,31 @@
+import { CheckInsRepoository } from '@/repositories/check-ins-repository'
+import { CheckIn } from '@prisma/client'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+interface ValidateCheckInUseCaseRequest {
+  checkInId: string
+}
+
+interface ValidateCheckInUseCaseResponse {
+  checkIn: CheckIn
+}
+
+export class ValidateCheckInUseCase {
+  constructor(private checkInRepository: CheckInsRepoository) {}
+
+  async execute({
+    checkInId,
+  }: ValidateCheckInUseCaseRequest): Promise<ValidateCheckInUseCaseResponse> {
+    const checkIn = await this.checkInRepository.findById(checkInId)
+
+    if (!checkIn) {
+      throw new ResourceNotFoundError()
+    }
+
+    checkIn.validated_at = new Date()
+
+    await this.checkInRepository.save(checkIn)
+
+    return { checkIn }
+  }
+}
